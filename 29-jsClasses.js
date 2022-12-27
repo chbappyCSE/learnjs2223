21eafab325b3b6c584dab4952273960e68f622e6
bappi <bappi.chandra@amiprobashi.com>
2022-12-27 15:42:10 +0600

    practice class

--- 29-jsClasses.js
+++ 29-jsClasses.js
@@ -12,6 +12,7 @@
         constructor() {... }
     }
  */
+/* 
 
 // Class property name, age
 class Car {
@@ -41,3 +42,92 @@ const person2 = new person("Tamim", 36); // create actual object
 
 person1.play("Well");
 person2.play("Good");
+ */
+/* class Student {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    sub(subj) {
+        console.log(`${this.name} is a ${subj} student. And his age is ${this.age}.`);
+    }
+}
+const student1 = new Student("Bappy", 25);
+const student2 = new Student("Wasi", 35);
+student1.sub("CSE");
+student2.sub("Science"); */
+
+// Another example
+/* class Car {
+    constructor(name, year) {
+        this.name = name;
+        this.year = year;
+
+    }
+    age() {
+        let date = new Date();
+        return date.getFullYear() - this.year;
+    }
+}
+let myCar = new Car("Ford", 2014);
+console.log(`My ${myCar.name} is ${myCar.age()} years old.`); */
+
+// My age
+/* class Ch {
+    constructor(name, year) {
+        this.name = name;
+        this.year = year;
+
+    }
+    age() {
+        let date = new Date();
+        return date.getFullYear() - this.year;
+    }
+}
+let myAge = new Ch("Bappy", 1997);
+console.log(`${myAge.name} is ${myAge.age()} years old.`); */
+
+// Another way
+/* class My {
+    constructor(name, year) {
+        this.name = name;
+        this.year = year;
+    }
+    age(x) {
+        return x - this.year;
+    }
+}
+
+let date = new Date();
+let year = date.getFullYear();
+
+let bappyInfo = new My("CH Bappy", 2014);
+console.log(bappyInfo.name) */
+
+// class My {
+//     constructor(name, year) {
+//         this.name = name;
+//         this.year = year;
+//     }
+//     age(x) {
+//         return x - this.year;
+//     }
+// }
+// let date = new Date();
+// let year = date.getFullYear();
+// let myInfo = new My("Ch Bappy", 1996);
+// console.log(`${myInfo.name} is ${myInfo.age(year)} years old.`);
+
+class Candidate {
+    constructor(name, year) {
+        this.name = name;
+        this.year = year;
+    }
+    age(x) {
+        return x - this.year;
+    }
+}
+let date = new Date();
+let year = date.getFullYear();
+let candidateInfo = new Candidate("Alok Das", 1995);
+console.log(`${candidateInfo.name} is ${candidateInfo.age(year)} years old`);
---
+++ 30-jsJson.js
@@ -0,0 +1,20 @@
+/*
+ * JSON is a format for storing and transporting data.
+ * JSON is often used when data is sent from a server to a web page.
+ * What is JSON?
+    JSON stands for JavaScript Object Notation
+    JSON is a lightweight data interchange format
+    JSON is language independent *
+    JSON is "self-describing" and easy to understand 
+ * The JSON syntax is derived from JavaScript object notation syntax, but the JSON format is text only. Code for reading and generating JSON data can be written in any programming language.
+ */
+
+// JSON Example
+
+{
+    "employees": [
+        { "firstName": "John", "lastName": "Doe" },
+        { "firstName": "Anna", "lastName": "Smith" },
+        { "firstName": "Peter", "lastName": "Jones" }
+    ]
+}
