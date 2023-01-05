ab9b25f39742e5bfaddeaf6c29a18079f9e6e68f
bappi <bappi.chandra@amiprobashi.com>
2023-01-05 11:17:42 +0600

    start work with advnced function

---
+++ 03-hard-part/02-js-function/js-function.js
@@ -0,0 +1,15 @@
+/* 
+ * JavaScript Function Definations 
+ *
+ * JavaScript functions are defined with the function keyword
+ * You can use a function declaration or a function expression
+ * 
+ */
+
+// Function Declarations
+/* 
+function functionName(perameters) {
+    // code to be executed
+}
+
+*/
